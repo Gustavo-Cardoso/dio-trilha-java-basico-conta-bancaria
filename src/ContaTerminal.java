8b0f9e9c223c25355dba78f6e6e16c8a91f5e593
Gustavo Cardoso <cardosogustavo790@gmail.com>
2026-03-11 22:23:13 -0300

    Finalizando desafio DIO - Criar um simulador de conta bancária através do terminal/console

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Exibir as mensagens para o nosso usuário e bter pelo scanner os valores digitados no terminal
+        System.out.println("Por favor, informe o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, informe o número da agência: ");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("Por favor, informe o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, informe o valor a ser depositado: ");
+        double saldoCliente = scanner.nextDouble();
+
+        scanner.close(); // Fechar o Scanner é uma boa prática
+
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoCliente + " já está disponível para saque.");
     }
 }
